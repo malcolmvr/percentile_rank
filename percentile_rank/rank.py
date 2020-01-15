94eb41fe14078cf6693e89148a6fb36f58029a4f
Malcolm van Raalte <malcolm@gainx.com>
2020-01-15 13:01:21 -0500

    renamed percentRank to percent_rank

--- percentile_rank/rank.py
+++ percentile_rank/rank.py
@@ -1,4 +1,4 @@
-def percentRank(arr, value):
+def percent_rank(arr, value):
 
     for i in range(len(arr)):
         if (arr[i] == value):
@@ -8,8 +8,8 @@ def percentRank(arr, value):
         if (arr[i] < value and value < arr[i + 1]):
             x1 = arr[i]
             x2 = arr[i + 1]
-            y1 = percentRank(arr, x1)
-            y2 = percentRank(arr, x2)
+            y1 = percent_rank(arr, x1)
+            y2 = percent_rank(arr, x2)
             return (((x2 - value) * y1 + (value - x1) * y2)) / (x2 - x1)
 
     raise Exception('Out of bounds')
